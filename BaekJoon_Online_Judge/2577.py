5bfe1b6cbbbb34ac5df788f6236a1fe83ed4455a
heeseok <dldydldy@naver.com>
2020-05-15 15:47:19 +0900

    2577.py

---
+++ BaekJoon_Online_Judge/2577.py
@@ -0,0 +1,35 @@
+import sys
+
+count_list = [0]*10
+print(len(count_list))
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+c = int(sys.stdin.readline())
+
+x = str(a*b*c)
+
+for i in x:
+    if i == '0':
+        count_list[0] += 1
+    elif i == '1':
+        count_list[1] += 1
+    elif i == '2':
+        count_list[2] += 1
+    elif i == '3':
+        count_list[3] += 1
+    elif i == '4':
+        count_list[4] += 1
+    elif i == '5':
+        count_list[5] += 1
+    elif i == '6':
+        count_list[6] += 1
+    elif i == '7':
+        count_list[7] += 1
+    elif i == '8':
+        count_list[8] += 1
+    elif i == '9':
+        count_list[9] += 1
+
+for i in count_list:
+    print(i)
