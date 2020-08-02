85c8764b47329ef2a577d1e3c3e8a26e62189723
heeseok <dldydldy@naver.com>
2020-08-02 17:50:01 +0900

    2447.py

---
+++ BaekJoon_Online_Judge/2447.py
@@ -0,0 +1,22 @@
+def solve(n, x, y): #9
+    if n == 3:
+        for i in range(x, x+3):
+            for j in range(y, y+3):
+                if not (i%3 == 1 and j%3 == 1):
+                    star[i][j] = '*'
+        return#9,0,9
+    for i in range(x, x+n, n//3): #0, 3, 6
+        for j in range(y, y+n, n//3): #9, 12, 15
+            if not (i-x == n//3 and j-y == n//3): #3,12 일때 막아야 함
+                solve(n//3, i, j)
+
+
+
+n = int(input())
+star = [[" " for _ in range(n)] for _ in range(n)]
+x, y = 0, 0
+solve(n, x, y)
+for i in range(n):
+    for j in range(n):
+        print(star[i][j], end='')
+    print()
--- BaekJoon_Online_Judge/test.py
+++ BaekJoon_Online_Judge/test.py
@@ -1,103 +1,21 @@
-# def solution(n, m, dusts, walls, winds):
-#     answer = 0
-#     mat = [[0 for col in range(m)] for row in range(n)]
-#     # set walls
-#     mat = setWalls(mat, walls)
-#     # set dusts
-#     mat = setDusts(mat, dusts)
-#     # print("Before :", str(mat))
-#     # extend dusts
-#     ext(mat, winds, n, m)
-#     # check num
-#     answer = checkNum(mat)
-#
-#     # print("After  :", str(mat))
-#     return answer
-#
-# def setWalls(mat, walls):
-#     for i in walls:
-#         mat[i[0]-1][i[1]-1] = 2
-#     return mat
-#
-# def setDusts(mat, dusts):
-#     for i in dusts:
-#         mat[i[0]-1][i[1]-1] = 1
-#     return mat
-#
-# def ext(mat, winds, n, m):
-#     for a in winds:
-#         temp = []
-#         if a == 'E':
-#             for i in range(0, n):
-#                 for j in range(0, m):
-#                     if j>0:
-#                         if mat[i][j] == 1 and mat[i][j-1] != 2:
-#                             temp.append((i, j-1))
-#         elif a == 'W':
-#             for i in range(0, n):
-#                 for j in range(0, m):
-#                     if j<m-1:
-#                         if mat[i][j] == 1 and mat[i][j+1] != 2:
-#                             temp.append((i, j+1))
-#         elif a == 'S':
-#             for i in range(0, n):
-#                 if i>0:
-#                     for j in range(0, m):
-#                         if mat[i][j] == 1 and mat[i-1][j] != 2:
-#                                 # print(i , j)
-#                                 temp.append((i-1, j))
-#         else:
-#             for i in range(0, n):
-#                 if i<n-1:
-#                     for j in range(0, m):
-#                         if mat[i][j] == 1 and mat[i+1][j] != 2:
-#                             temp.append((i+1, j))
-#         # print(temp)
-#         for b in temp:
-#             mat[b[0]][b[1]] = 1
-#         # print(a, str(mat))
-#         # print()
-#     return mat
-#
-# def checkNum(mat):
-#     count = 0
-#     for i in mat:
-#         for j in i:
-#             if j == 1:
-#                 count += 1
-#     return count
-#
-#
-# n = 3
-# m =  3
-# dusts =  [[1,3],[3,1]]
-# walls =  [[2,2]]
-# winds =  ["W", "S", "S", "E", "N"]
-# print(solution(n, m, dusts, walls, winds))
+import sys
 
+def read_sys():
+    return sys.stdin.readline()
 
-def solution(ages, wires):
-    temp = ages
-    m = max(ages)
-    answer = []
-    state = [1] * len(ages)
+def solve():
+    time = 0
+    count = 0
+    for x, y in sorted_time_list:
+        if time <= x:
+            time = y
+            count += 1
+    return count
 
-
-    for i in range(1, m+1):
-        for j in range(0, len(ages)):
-            temp[j] -= 1
-            if temp[j] <= 0 and state[j] == 1:
-                for x in wires:
-                    if x[1] == j:
-                        if temp[x[0]]+x[2] > 0:
-                            state[j] = 1
-                        else:
-                            state[j] = 0
-                            answer.append(j+1)
-
-
-    return answer
-
-ages = [35,25,3,8,7]
-wires = [[1,2,5],[2,1,5],[1,3,2],[3,4,2],[3,5,20],[4,5,1]]
-print(solution(ages, wires))
+n = int(read_sys())
+time_list = [list(map(int, read_sys().split())) for _ in range(n)]
+sorted_time_list = sorted(time_list, key = lambda x : (x[1], x[0]))
+print(time_list)
+print(sorted_time_list)
+count = solve()
+print(count)
