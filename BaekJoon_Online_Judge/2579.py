6c633cdcd66cdbec13ba59a00b67f00004558de4
heeseok <dldydldy@naver.com>
2020-08-05 17:54:11 +0900

    solve 2579.py

--- BaekJoon_Online_Judge/2579.py
+++ BaekJoon_Online_Judge/2579.py
@@ -1,37 +1,17 @@
 def solve():
-# 반례, 거꾸로 찾으면 안될듯 순서대로 찾자
-# 5
-# 50
-# 40
-# 30
-# 20
-# 10
-    result = n_list[length-1]
-    memo.append(n_list[length-1])
-    count = 1
-    for i in range(length-1, 0, -1):
-        print(i, ",", memo, ", count :", count, ", i-1 :", n_list[i-1], ", i-2 :", n_list[i-2])
-        if count == 2:
-            count = 0
-            continue
-        if i == 1:
-            memo.append(n_list[i-1])
-            result += n_list[i-1]
-
-        elif n_list[i-1] >= n_list[i-2]:
-            count += 1
-            memo.append(n_list[i-1])
-            result += n_list[i-1]
-        else:
-            count = 2
-            memo.append(n_list[i-2])
-            result += n_list[i-2]
-
-    return result
+    return
 
 length = int(input())
-n_list = [int(input()) for _ in range(length)]
-# memo = [0 for _ in range(length)]
-memo = []
-result = solve()
-print(result)
+s_list = [int(input()) for _ in range(length)]
+memo = [0 for _ in range(length)]
+memo[0] = s_list[0]
+if length == 1:
+    print(s_list[0])
+elif length == 2:
+    print(s_list[0] + s_list[1])
+else:
+    memo[1] = s_list[0] + s_list[1]
+    memo[2] = max(s_list[0]+s_list[2], s_list[1]+s_list[2])
+    for i in range(3, length):
+        memo[i] = max(memo[i-3]+s_list[i-1]+s_list[i], memo[i-2]+s_list[i])
+    print(memo[length-1])
