5a3faf179980232ffa53dcac89f512be966165e0
heeseok <dldydldy@naver.com>
2020-08-24 16:05:48 +0900

    solve 11866.py

--- BaekJoon_Online_Judge/11866.py
+++ BaekJoon_Online_Judge/11866.py
@@ -2,17 +2,32 @@ from collections import deque
 
 def solve():
     # queue = deque(i for i in range(1, n+1))
-    queue = [i for i in range(1, n+1)]
+    queue = [i for i in range(0, n+1)]
     temp = []
     index = 0
-    for i in range(n):
-        if len(queue) == 1:
-            temp.append(str(queue[0]))
-        else:
-            
+    check = 0
+    times = 0
+    while times != n-1:
+        for _ in range(k):
+            check += 1
+            if index == len(queue)-1:
+                 index = 1
+            else:
+                index += 1
+        if check == k:
+            x = queue[index]
+            temp.append(str(x))
+            del queue[index]
+            check = 0
+            index -= 1
+            times += 1
+
+    temp.append(str(queue[1]))
+    return temp
+
 
 n, k = map(int, input().split())
-solve()
+temp = solve()
 result = '<'
 result += ', '.join(temp)
 result += '>'
