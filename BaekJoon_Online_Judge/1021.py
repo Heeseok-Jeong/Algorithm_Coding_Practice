e6f9461d3f4a3eec79a56f83c49cefb1d8b7ea04
heeseok <dldydldy@naver.com>
2020-08-27 17:10:30 +0900

    1021.py

--- BaekJoon_Online_Judge/1021.py
+++ BaekJoon_Online_Judge/1021.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 def move_r(list_):
-    list_.appendleft(len(list_)-1)
+    list_.appendleft(list_[len(list_)-1])
     list_.pop()
     return list_
 
@@ -13,18 +13,22 @@ def move_l(list_):
 def solve(deq_):
     result = 0
     for x in info:
-        idx = deq_.index(x)
-        left = len(deq_)-idx-1
-        right = len(deq_)-left
-        if left < right:
-            for i in range(left):
-                deq_ = move_l(deq_)
-                result += 1
+        if x == deq_[0]:
+            deq_.popleft()
+            continue
         else:
-            for i in range(right):
-                deq_ = move_l(deq_)
-                result += 1
-
+            idx = deq_.index(x)
+            right = len(deq_)-idx
+            left = len(deq_)-right
+            if left < right:
+                for i in range(left):
+                    deq_ = move_l(deq_)
+                    result += 1
+            else:
+                for i in range(right):
+                    deq_ = move_r(deq_)
+                    result += 1
+            deq_.popleft()
     return result
 
 n, m = map(int, input().split())
