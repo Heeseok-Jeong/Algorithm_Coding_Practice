ebc40c9cb088c15023b217f3da727480bef18364
heeseok <dldydldy@naver.com>
2020-10-17 00:59:05 +0900

    update grammar

---
+++ Python_Grammar/list_functions.py
@@ -0,0 +1,10 @@
+my_str = "Hello World!"
+
+print(min(my_str))
+print(max(my_str))
+print(my_str.count('l')) #3
+print(my_str.find('o')) #4
+print(my_str.isalnum()) #
+print(my_str.isalpha())
+print(my_str.isdigit())
+print(my_str.isdecimal())
