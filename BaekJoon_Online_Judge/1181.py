0c9e207ef56ab4cadd6136b03ba47113c0d73811
Heeseok-Jeong <dldydldy@naver.com>
2021-03-08 23:04:28 +0900

    Add 1181.py

---
+++ BaekJoon_Online_Judge/1181.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+words = list(set([input().rstrip() for _ in range(N)]))
+
+words.sort(key=lambda x : (len(x), x))
+
+for word in words:
+    print(word)
