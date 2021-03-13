69e71888ffadebccab62e6b071c79290222d4d6f
Heeseok-Jeong <dldydldy@naver.com>
2021-03-13 23:56:31 +0900

    Update 1074.py

--- BaekJoon_Online_Judge/1074.py
+++ BaekJoon_Online_Judge/1074.py
@@ -1,4 +1,4 @@
-import math
+from math import log2
 
 N, r, c = map(int, input().split())
 
@@ -6,38 +6,26 @@ grid = [[0 for _ in range(2**N)] for _ in range(2**N)]
 
 def make_z_grid(n, x, y):
     if n == 1:
-        x_value, y_value = 0, 0
-        if x != 0:
-            e_x = int(math.log(x, 2))
-            x_value = 2**(2*e_x + 1) if 2**e_x == x else grid[e_x][y] + grid[x-e_x][y]
-            
-        if y != 0:
-            e_y = int(math.log(y, 2))
-            y_value = 2**(2*e_y) if 2**e_y == y else grid[x][e_y] + grid[x][y-e_y]
-
         base = 0
         if not (x == 0 and y == 0):
-            e_x, e_y = 0, 0
-            if x != 0:
-                e_x = int(math.log(x, 2))
-            if y != 0:
-                e_y = int(math.log(y, 2))
-            base = grid[x-e_x][y-e_y] + grid[e_x][e_y]
-
+            if x == 0:
+                log_y = log2(y)
+                if log_y == int(log_y):
+                    base = 2**y
+            elif y == 0:
+                log_x = log2(x)
+                if log_x == int(log_x):
+                    base = 2**(x+1)
+            else:
+                log_x = log2(x)
+                log_y = log2(y)
+                base = 
         grid[x][y] = base
-        grid[x][y+1] = base+1
-        grid[x+1][y] = base+2
-        grid[x+1][y+1] = base+3
+        grid[x][y+1] = base + 1
+        grid[x+1][y] = base + 2
+        grid[x+1][y+1] = base + 3
 
-    # if n == 0:
-        # if x != 0:
-        #     e_x = int(math.log(x, 2))
-        # if y != 0:
-        #     e_y = int(math.log(y, 2))
 
-    #     if x == 0 and y == 0:
-
-    #     grid[x][y] = 
     else:
         make_z_grid(n-1, x, y)
         make_z_grid(n-1, x, y+2**(n-1))
@@ -50,3 +38,4 @@ for line in grid:
     print(line)
 
 print(grid[r][c])
+
