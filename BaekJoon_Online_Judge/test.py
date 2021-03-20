5a3e90937e582b72e26adc15ce692c60b3659302
Heeseok-Jeong <dldydldy@naver.com>
2021-03-21 00:35:00 +0900

    Check test.py

--- BaekJoon_Online_Judge/test.py
+++ BaekJoon_Online_Judge/test.py
@@ -1,8 +1,23 @@
-sum = 0
+def z(n, x, y):
+    global result, N, flag
+    if x == r and y == c:
+        flag = True
+        return
+    if flag:
+        return
+    
+    if n == 0:
+        result += 1
+    elif not (x <= r < x+2**n and y <= c < y+2**n) and n != N:
+        result += 4**n
+    else:    
+        z(n-1, x, y)
+        z(n-1, x, y + 2**(n-1))
+        z(n-1, x + 2**(n-1), y)
+        z(n-1, x + 2**(n-1), y + 2**(n-1))
 
-print(type(sum))
-for i in range(7):
-    sum += 1/7
-print(sum, sum == 1.0)
-print(int(sum))
-print(type(sum))
+N, r, c = map(int, input().split())
+result = 0
+flag = False
+z(N, 0, 0)
+print(result)
