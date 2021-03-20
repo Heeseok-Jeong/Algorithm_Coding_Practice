a53bc2cada6ed4155ed5473841a3f9eb1b732dd3
Heeseok-Jeong <dldydldy@naver.com>
2021-03-21 04:03:41 +0900

    Add 1991.py

--- BaekJoon_Online_Judge/1991.py
+++ BaekJoon_Online_Judge/1991.py
@@ -2,51 +2,32 @@ import sys
 
 input = sys.stdin.readline
 
-tree = ['.' for _ in range(2*(26+2))]
+tree = {}
 result = []
-
-def alpha2index(a):
-    for i in range(2*(26+2)):
-        if tree[i] == a:
-            break
-    # print(a, i)
-    return i
 
 def preorder_traversal(a):
     if a == '.':
         return
     
-    if alpha2index(a)*2 >= 2*(26+1):
-        result.append(a)
-        return
-
     result.append(a)
-    preorder_traversal(tree[alpha2index(a)*2])
-    preorder_traversal(tree[alpha2index(a)*2 + 1])
+    preorder_traversal(tree[a][0])
+    preorder_traversal(tree[a][1])
 
 
 def inorder_traversal(a):
     if a == '.':
         return
     
-    if alpha2index(a)*2 >= 2*(26+1):
-        result.append(a)
-        return
-
-    inorder_traversal(tree[alpha2index(a)*2])
+    inorder_traversal(tree[a][0])
     result.append(a)
-    inorder_traversal(tree[alpha2index(a)*2 + 1])
+    inorder_traversal(tree[a][1])
 
 def postorder_traversal(a):
     if a == '.':
         return
 
-    if alpha2index(a)*2 >= 2*(26+1):
-        result.append(a)
-        return
-
-    postorder_traversal(tree[alpha2index(a)*2])
-    postorder_traversal(tree[alpha2index(a)*2 + 1])
+    postorder_traversal(tree[a][0])
+    postorder_traversal(tree[a][1])
     result.append(a)
 
 def print_result():
@@ -56,12 +37,9 @@ def print_result():
     result.clear()
 
 N = int(input())
-tree[1] = "A"
 for _ in range(N):
     n, l, r = input().split()
-    tree[alpha2index(n)*2] = l
-    tree[alpha2index(n)*2 + 1] = r
-# print(tree)
+    tree[n] = (l, r)
     
 preorder_traversal("A")
 print_result()
