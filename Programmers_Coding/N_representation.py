2f78218344db85fe2bc7ae243940f4ca6ea69564
Heeseok-Jeong <dldydldy@naver.com>
2021-08-23 02:58:15 +0900

    Update N_representation.py

--- Programmers_Coding/N_representation.py
+++ Programmers_Coding/N_representation.py
@@ -1,17 +1,27 @@
 def solution(N, number):
     answer = 0
-    memo = [-1] * 32000
-    memo[0] = [0, [0]]
-    count = 0
-    count += 1
-    memo[N] = [count, [5]]
+    cases = [0, [N]]
     if N == number:
-        return memo[N][0]
+        return 1
     
-    q = [0]
-    while q:
-        count += 1
-        n = q.pop()
+    for count in range(2, 9):
+        temp_cases = [int(str(N)*count)]
+        for i in range(1, count//2 + 1):
+            for x in cases[i]:
+                for y in cases[count-i]:
+                    temp_cases.append(x+y)
+                    temp_cases.append(x-y)
+                    temp_cases.append(y-x)
+                    temp_cases.append(x*y)
+                    if y != 0:
+                        temp_cases.append(x//y)
+                    if x != 0:
+                        temp_cases.append(y//x)
+        if number in temp_cases:
+            return count
+        cases.append(temp_cases)
         
-        
-    return answer
+    return -1
+
+N, number = map(int, input().split())
+print(solution(N, number))
